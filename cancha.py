b60cc2f89eee72b4c63e076c98d13ac278ac833b
Omar Fermoselle Reina <omar270215@hotmail.com>
2024-05-10 20:07:50 +0200

    Estructura de Cancha

--- cancha.py
+++ cancha.py
@@ -1,2 +1,19 @@
 class Cancha:
-    pass
+    def __init__(self, numero_cancha, deporte, habilitada, lista_reserva, lista_empleados ):
+        self.numero_cancha = numero_cancha
+        self.deporte = deporte
+        self.habilitada = habilitada
+        self.lista_reserva = list(lista_reserva)
+        self.lista_empleado = list(lista_empleados)
+
+    def crear_cancha(self):
+        pass
+
+    def agregar_cancha(self):
+        pass
+
+    def listar_canchas(self):
+        pass
+
+    def quitar_cancha(self):
+        pass

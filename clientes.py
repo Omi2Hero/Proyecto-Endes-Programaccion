69b992f4a2a3b78008bfc8d3eaa4a6a5cce0a33c
aaantonioo <danielmolina43211@gmail.com>
2024-05-14 16:03:23 +0200

    Prueba commit

---
+++ clientes.py
@@ -0,0 +1,11 @@
+class Clientes:
+    def __init__(self, nombre, apellido, telefono, identificador, activo):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.identifcador = identificador
+    
+    def prueba():
+        pass
+
+    
